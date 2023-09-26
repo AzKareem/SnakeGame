f6ea3e8c1ec805346b4912c6dc4c0de580133ee5
Karim <karim-azimi@hotmail.com>
2023-09-26 13:07:03 +0200

    the first steps
    added the field and renderFunction

---
+++ src/FieldType.java
@@ -0,0 +1,14 @@
+public enum FieldType {
+    EMPTY(0," "),
+    SNAKE(10, "*"),
+    FRUIT(20,"+");
+
+    int value;
+    String display;
+    FieldType(int value, String display){
+        this.value = value;
+        this.display = display;
+    }
+
+
+}
